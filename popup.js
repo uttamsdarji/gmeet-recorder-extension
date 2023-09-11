ae1f7538ad7860c8b4e18728f1fb2c51f47370f4
Uttam <uttam@wareiq.com>
2023-09-11 08:19:19 +0530

    tab fixes

--- popup.js
+++ popup.js
@@ -1,21 +1,21 @@
 // popup.js - JavaScript for the popup UI
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
   // Get references to HTML elements
   const startRecordingButton = document.getElementById("startRecording");
   const stopRecordingButton = document.getElementById("stopRecording");
   const transcriptDiv = document.getElementById("transcript");
-
+  let tabs = await chrome.tabs.query({ active: true, currentWindow: true });
   // Add click event listeners to the buttons
   startRecordingButton.addEventListener("click", function () {
     // Send a message to the background script to start recording
-    chrome.runtime.sendMessage({ action: "startRecording" }, function (response) {
+    chrome.runtime.sendMessage({ action: "startRecording", tabId: tabs?.[0]?.id }, function (response) {
       console.log("Recording started:", response);
     });
   });
 
   stopRecordingButton.addEventListener("click", function () {
     // Send a message to the background script to stop recording
-    chrome.runtime.sendMessage({ action: "stopRecording" }, function (response) {
+    chrome.runtime.sendMessage({ action: "stopRecording", tabId: tabs?.[0]?.id }, function (response) {
       console.log("Recording stopped:", response);
     });
   });
